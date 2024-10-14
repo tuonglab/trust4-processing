2c556fdb83d112eb662c069952f7ddc793997b91
amos <amoschoouqconnect@gmail.com>
2024-10-14 15:47:39 +1000

    update trust4 script (pending confirmation)

--- trust4_run_fastq.sh
+++ trust4_run_fastq.sh
@@ -2,83 +2,84 @@
 
 # Check if a directory path, an output directory, a reference file, and a processed files file were provided as arguments
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 directory_path output_dir reference_file"
+    echo "Usage: $0 directory_path output_dir fasta_file reference_genome"
     exit 1
 fi
 
 # Set the directory path, output directory, reference file to the first, second, third respectively
 dir_path="$2"
 output_dir="$1"
-reference_file="$3"
+fasta_file="$3"
+reference_genome="$4"
 
 # Create new directory
 mkdir -p "$output_dir"
 
-# Find pairs of FASTQ files in the specified directory using the _R1.fastq.gz and _R2.fastq.gz naming convention
-for r1_file in "$dir_path"/*_R1.fastq.gz; do
-    r2_file="${r1_file%_R1.fastq.gz}_R2.fastq.gz"
-    if [ -f "$r2_file" ]; then
+# # Find pairs of FASTQ files in the specified directory using the _R1.fastq.gz and _R2.fastq.gz naming convention
+# for r1_file in "$dir_path"/*_R1.fastq.gz; do
+#     r2_file="${r1_file%_R1.fastq.gz}_R2.fastq.gz"
+#     if [ -f "$r2_file" ]; then
 
-        # Copy FASTQ files to $TMPDIR
-        rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
+#         # Copy FASTQ files to $TMPDIR
+#         rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
 
-        # Process FASTQ files in $TMPDIR
-        dir=$(basename "${r1_file%_R1.fastq.gz}")
-        output_subdir="$output_dir/$dir"
-        mkdir -p "$output_subdir"
-        echo "Processing $r1_file and $r2_file"
+#         # Process FASTQ files in $TMPDIR
+#         dir=$(basename "${r1_file%_R1.fastq.gz}")
+#         output_subdir="$output_dir/$dir"
+#         mkdir -p "$output_subdir"
+#         echo "Processing $r1_file and $r2_file"
 
-        # Run trust4 on the FASTQ files and save output to output_subdir
-        /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$reference_file" -t 64 --od "$output_subdir"
+#         # Run trust4 on the FASTQ files and save output to output_subdir
+#         /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$fasta_file" -t 64 --od "$output_subdir"
 
-    fi
-done
+#     fi
+# done
 
-# Find pairs of FASTQ files in the specified directory using the _R1.fq.gz and _R2.fq.gz naming convention
-for r1_file in "$dir_path"/*_R1.fq.gz; do
-    r2_file="${r1_file%_R1.fq.gz}_R2.fq.gz"
-    if [ -f "$r2_file" ]; then
+# # Find pairs of FASTQ files in the specified directory using the _R1.fq.gz and _R2.fq.gz naming convention
+# for r1_file in "$dir_path"/*_R1.fq.gz; do
+#     r2_file="${r1_file%_R1.fq.gz}_R2.fq.gz"
+#     if [ -f "$r2_file" ]; then
 
-        # Copy FASTQ files to $TMPDIR
-        rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
+#         # Copy FASTQ files to $TMPDIR
+#         rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
 
-        # Process FASTQ files in $TMPDIR
-        dir=$(basename "${r1_file%_R1.fq.gz}")
-        output_subdir="$output_dir/$dir"
-        mkdir -p "$output_subdir"
-        echo "Processing $r1_file and $r2_file"
+#         # Process FASTQ files in $TMPDIR
+#         dir=$(basename "${r1_file%_R1.fq.gz}")
+#         output_subdir="$output_dir/$dir"
+#         mkdir -p "$output_subdir"
+#         echo "Processing $r1_file and $r2_file"
 
-        # Run trust4 on the FASTQ files and save output to output_subdir
-        /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$reference_file" -t 64 --od "$output_subdir"
+#         # Run trust4 on the FASTQ files and save output to output_subdir
+#         /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$fasta_file" -t 64 --od "$output_subdir"
 
-        # Copy output files back to output_subdir
-        rsync -avz "$output_subdir/"* "$output_subdir/" --progress
-
-
-    fi
-done
-
-# Find pairs of FASTQ files in the specified directory using the _F.fq.gz and _R.fq.gz naming convention
-for r1_file in "$dir_path"/*_F.fq.gz; do
-    r2_file="${r1_file%_F.fq.gz}_R.fq.gz"
-    if [ -f "$r2_file" ]; then
+#         # Copy output files back to output_subdir
+#         rsync -avz "$output_subdir/"* "$output_subdir/" --progress
 
 
-        # Copy FASTQ files to $TMPDIR
-        rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
+#     fi
+# done
 
-        # Process FASTQ files in $TMPDIR
-        dir=$(basename "${r1_file%_F.fq.gz}")
-        output_subdir="$output_dir/$dir"
-        mkdir -p "$output_subdir"
-        echo "Processing $r1_file and $r2_file"
-
-        # Run trust4 on the FASTQ files and save output to output_subdir
-        /home/uqachoo1/mambaforge/envs/env/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$reference_file" -t 64 --od "$output_subdir"
+# # Find pairs of FASTQ files in the specified directory using the _F.fq.gz and _R.fq.gz naming convention
+# for r1_file in "$dir_path"/*_F.fq.gz; do
+#     r2_file="${r1_file%_F.fq.gz}_R.fq.gz"
+#     if [ -f "$r2_file" ]; then
 
 
-    fi
-done
+#         # Copy FASTQ files to $TMPDIR
+#         rsync -avz "$r1_file" "$r2_file" "$TMPDIR/" --progress
+
+#         # Process FASTQ files in $TMPDIR
+#         dir=$(basename "${r1_file%_F.fq.gz}")
+#         output_subdir="$output_dir/$dir"
+#         mkdir -p "$output_subdir"
+#         echo "Processing $r1_file and $r2_file"
+
+#         # Run trust4 on the FASTQ files and save output to output_subdir
+#         /home/uqachoo1/mambaforge/envs/env/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$fasta_file" -t 64 --od "$output_subdir"
+
+
+#     fi
+# done
 
 # Loop through all subdirectories in the specified directory
 for subdir in "$dir_path"/*/; do
@@ -97,7 +98,7 @@ for subdir in "$dir_path"/*/; do
         echo "Processing $r1_file and $r2_file"
 
         # Run trust4 on the FASTQ files and save output to output_subdir
-        /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$reference_file" -t 64 --od "$output_subdir"
+        /scratch/user/uqachoo1/miniforge3/bin/run-trust4 -1 "$TMPDIR/$(basename "$r1_file")" -2 "$TMPDIR/$(basename "$r2_file")" -f "$fasta_file" --ref "$reference_genome" -t 64 --od "$output_subdir"
 
     fi
 done
--- trust4_run_fastq_batch.sh
+++ trust4_run_fastq_batch.sh
@@ -2,18 +2,20 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=256G
 #SBATCH --job-name=trust4_fastq
-#SBATCH --time=3:00:00
+#SBATCH --time=300:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_kelvin_tuong
-#SBATCH -o trust4run_fastq_phs002599.output
-#SBATCH -e trust4run_fastq_phs002599.error
+#SBATCH -o trust4run_fastq_sra.output
+#SBATCH -e trust4run_fastq_sra.error
 
 # Set variables
-dir_path="$2"
-output_dir="$1"
-reference_file="$3"
+dir_path="/QRISdata/Q7361/SRRIDS/fastqfilesncbi"
+output_dir="scratch/project/tcr_ml/trust4_sra"
+fasta_file="/scratch/project/tcr_ml/trust4-processing/hg38_bcrtcr.fa"
+reference_genome=="/scratch/project/tcr_ml/trust4-processing/human_IMGT+C.fa"
 
-srun trust4_run_fastq.sh "$output_dir" "$dir_path" "$reference_file"
+
+srun trust4_run_fastq.sh "$output_dir" "$dir_path" "$fasta_file" "$reference_genome"
