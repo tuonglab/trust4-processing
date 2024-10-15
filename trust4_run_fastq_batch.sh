42b9b7da25ab98a06738c9ad48140ba89de089f6
amos <amoschoouqconnect@gmail.com>
2024-10-16 09:49:02 +1000

    fix typo error in path

--- trust4_run_fastq_batch.sh
+++ trust4_run_fastq_batch.sh
@@ -15,7 +15,7 @@
 dir_path="/QRISdata/Q7361/SRRIDS/fastqfilesncbi"
 output_dir="/scratch/project/tcr_ml/trust4_sra"
 fasta_file="/scratch/project/tcr_ml/trust4-processing/hg38_bcrtcr.fa"
-reference_genome=="/scratch/project/tcr_ml/trust4-processing/human_IMGT+C.fa"
+reference_genome="/scratch/project/tcr_ml/trust4-processing/human_IMGT+C.fa"
 
 
 srun trust4_run_fastq.sh "$output_dir" "$dir_path" "$fasta_file" "$reference_genome"
